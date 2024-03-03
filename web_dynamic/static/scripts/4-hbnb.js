e0a991502ef6e65991375445da5496969b3ab205
CharlesOkechukwu <realcharlieok@gmail.com>
2024-03-03 15:18:04 -0800

    filter places using amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,48 @@
+$('document').ready(function () {
+  const amenities = {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  const url = 'http://0.0.0.0:5001/api/v1/places_search/';
+  $('button').click(function () {
+    $.post(url,
+      JSON.stringify({ amenities: Object.keys(amenities) }),
+      append, 'json');
+  });
+  function append (data) {
+    for (const place in data) {
+      const template = `<article>
+      <div class="title">
+       <h2>${place.name}</h2>
+       <div class="price_by_night">
+       ${place.price_by_night}
+       </div>
+      </div>
+      <div class="information">
+       <div class="max_guest">
+       ${place.number_rooms} Bedrooms
+       </div>
+       <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+      </div>
+      <div class="description">
+      ${place.description}
+      </div>
+      </article>`;
+      $('section.places').append(template);
+    }
+  }
+});
